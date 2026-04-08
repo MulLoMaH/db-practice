54a2caa993beb70b75acdd383ab925e597397c2f
MulLoMaH <ssemvkin@list.ru>
2026-04-08 11:11:37 +0300

    request bd

---
+++ bd_postgres/simple_sql/simple_select.go
@@ -0,0 +1,62 @@
+package simple_sql
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+func SelectRows(ctx context.Context, conn pgx.Conn) error {
+	sqlQuery := `
+	SELECT id, title, description, completed, created_at, completed_at
+	FROM tasks
+	WHERE completed = FALSE;
+	`
+
+	// sqlQuery := ` - создаем запрос
+	// SELECT id, title, description, completed, created_at, completed_at - просим вернуть из
+	// базы данных параметры стольбцов id, title, description, completed, created_at, completed_at
+	// FROM tasks - из таблицы tasks
+	// WHERE completed = FALSE; // все строки у которых completed = FALSE
+	// `
+
+	rows, err := conn.Query(ctx, sqlQuery) //вернет все значения соответствующие условию
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close() // отложенное освобождение подключения к базе данных
+
+	for rows.Next() { //в цикле читаем значения полученные из conn.Query до
+		// конца (.Next() вернет по окончанию получения значений false и цикл остановиться)
+
+		// создаем переменные в которые получим значения
+		var id int
+		var title string
+		var description string
+		var completed bool
+		var created_at time.Time
+		var completed_at *time.Time
+
+		err := rows.Scan(
+			&id,
+			&title,
+			&description,
+			&completed,
+			&created_at,
+			&completed_at,
+		)
+
+		if err != nil {
+			return err
+		}
+
+		fmt.Println(id, title, description, completed, created_at, completed_at)
+	}
+
+	//conn.QueryRow() вернет вервую подходящую строку под условие
+
+	return nil
+}
